6f97f764c43b5c5e24f59e16322a242c25711113
Robert Cole <rjcole@usc.edu>
2025-07-15 10:17:57 -0700

    finish respawn script

--- Assets/Scripts/respawn.cs
+++ Assets/Scripts/respawn.cs
@@ -4,19 +4,30 @@ public class respawn : MonoBehaviour
 {
     public GameObject Arrows;
     public Transform SpawnPoint;
-    
+
+    private float timer = 0f;
+
     // Update is called once per frame
     void Update()
     {
 
+        timer += Time.deltaTime;
+
+        if (timer >= 2f)
+        {
             Spawner();
+            timer = 0f;
             
+        }
+
     }
 
     void Spawner()
     {
-        
+
         GameObject newplayer = Instantiate(Arrows, SpawnPoint.position, Quaternion.identity);
 
     }
+
+   
 }
